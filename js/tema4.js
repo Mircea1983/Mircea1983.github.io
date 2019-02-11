d66ffacb2e4907fa30a0b14c381b8aef00a05364
Mircea1983 <33032754+Mircea1983@users.noreply.github.com>
2019-02-11 09:43:12 +0200

    Update tema4.js

--- js/tema4.js
+++ js/tema4.js
@@ -2,7 +2,7 @@ var d = new Date ();
 document.getElementById("id_business_version").innerHTML = "Business version = " + 
 d.getFullYear() + "." + (d.getMonth()+ 1) + "." + d.getDate() + ".0";
 
-var vid = document.getElementById("https://www.youtube.com/watch?v=Qc-aNL6egpY&feature=share&fbclid=IwAR2EX-gpr5lTp6lCE7ZZhSUuTMLotc-b_vNCUdJDSc42t3SovxWBOq6sgkQ");
+var vid = document.getElementById("https://www.youtube.com/watch?v=Qc-aNL6egpY&feature=share&fbclid=IwAR1CbfWy_ROlA7J5zvPSmqKItj3x2x5y0P5sBN3yBWD8YW4ctVGeSB8eMjk");
 
 //...................................................
 function playVid() { 
