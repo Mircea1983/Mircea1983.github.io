09f1a13fb37927654c590075fe513f902309bf1c
Mircea1983 <33032754+Mircea1983@users.noreply.github.com>
2019-02-11 11:57:55 +0200

    Update tema4.js

--- js/tema4.js
+++ js/tema4.js
@@ -2,7 +2,7 @@ var d = new Date ();
 document.getElementById("id_business_version").innerHTML = "Business version = " + 
 d.getFullYear() + "." + (d.getMonth()+ 1) + "." + d.getDate() + ".0";
 
-var vid = document.getElementById("https://Alison Wonderland");
+var vid = document.getElementById("https://www.youtube.com/watch?v=Qc-aNL6egpY&feature=share&fbclid=IwAR33rZZ44qvpH6Dian0CaDR_fPzbMrnQiIM_sJg0par6P9yUDNyHvAcbZoY");
 
 //...................................................
 function playVid() { 
