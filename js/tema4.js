d02832a793ae6d727e6113f00a8d615a65ca5f6f
Mircea1983 <33032754+Mircea1983@users.noreply.github.com>
2019-02-11 14:40:33 +0200

    Update tema4.js

--- js/tema4.js
+++ js/tema4.js
@@ -2,15 +2,15 @@ var d = new Date ();
 document.getElementById("id_business_version").innerHTML = "Business version = " + 
 d.getFullYear() + "." + (d.getMonth()+ 1) + "." + d.getDate() + ".0";
 
-var vid = document.getElementById("https://youtu.be/Qc-aNL6egpY");
+//var vid = document.getElementById("https://youtu.be/Qc-aNL6egpY");
 
 //...................................................
-function playVid() { 
-    vid.play(); 
+//function playVid() { 
+    //vid.play(); 
 } 
 
-function pauseVid() { 
-    vid.pause(); 
+//function pauseVid() { 
+    //vid.pause(); 
 } 
 //...................................................
 
